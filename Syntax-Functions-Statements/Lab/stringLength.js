ce627628ac588245621eca836e6e295327977c07
Daniel Ivanov <d.ivanov1994@mail.bg>
2021-09-19 15:53:59 +0300

    New LAB uploaded.

---
+++ Syntax-Functions-Statements/Lab/stringLength.js
@@ -0,0 +1,9 @@
+function strLength(one, two, three) {
+
+    let total = one.length + two.length + three.length;
+    let avg = Math.floor(total / 3);
+
+    console.log(total);
+    console.log(avg);
+}
+strLength('pasta', '5', '22.3')
